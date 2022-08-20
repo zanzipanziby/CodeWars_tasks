d1f328800cd27131adc8075cd09b43166b5ae531
zanzipanziby <zanzipanziby@gmail.com>
2022-08-20 22:05:54 +0300

    del min

--- Удалить минимум.js
+++ Удалить минимум.js
@@ -1,8 +1,14 @@
 function removeSmallest(numbers) {
-   const newArr = numbers;
-   let minIndex = newArr.indexOf(Math.min.apply(null, newArr));
-   newArr.splice(minIndex, 1);
-   return newArr;
+    let newArr = Array.from(numbers);
+    if(numbers !== []) {
+    let minIndex = newArr.indexOf(Math.min.apply(null, newArr));
+    newArr.splice(minIndex, 1);
+    return newArr;
+  } else {
+    return [];
   }
+}
+
+
 
   console.log(removeSmallest([2, 1, 1, 1, 3]));
