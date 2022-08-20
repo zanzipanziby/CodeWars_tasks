8858cb946f76e2e4ada59c7ae56fb9971c0111d8
zanzipanziby <zanzipanziby@gmail.com>
2022-08-20 21:45:46 +0300

    Remove the minimum

--- Удалить минимум.js
+++ Удалить минимум.js
@@ -1,7 +1,8 @@
 function removeSmallest(numbers) {
-    const newArr = numbers;
-    let indexMinElem = indexOf(MAth.min(numbers));
-    return indexMinElem;
+   const newArr = numbers;
+   let minIndex = newArr.indexOf(Math.min.apply(null, newArr));
+   newArr.splice(minIndex, 1);
+   return newArr;
   }
 
-  console.log(removeSmallest([2,1,3]));
+  console.log(removeSmallest([2, 1, 1, 1, 3]));
