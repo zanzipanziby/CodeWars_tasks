151ea33d4857b55a9e0464ccf7b87a312e3db6c3
zanzipanziby <zanzipanziby@gmail.com>
2022-08-20 22:31:50 +0300

    Удалить строковые пробелы

---
+++ Удалить строковые пробелы.js
@@ -0,0 +1,14 @@
+/*function noSpace(x){
+    const arr = [];
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== " ") {
+        arr.push(x[i]);
+    }
+  }
+  return arr.join(undefined);
+}*/
+
+function noSpace(x){
+   return (x.split(' ')).join('');
+}
+console.log(noSpace('abb abbb bbbba'));
