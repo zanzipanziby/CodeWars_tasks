5f3bafe04595bd82230ab4242174d807ff2c84a0
ZanziPanziBY <zanzipanziby@gmail.com>
2022-08-22 13:29:27 +0300

    commit

--- Grasshopper - Grade book.js
+++ Grasshopper - Grade book.js
@@ -21,3 +21,11 @@ function getGrade(s1, s2, s3) {
         return 'A';
     }
 }
+
+
+/**VAR 2 */
+
+function getGrade (s1, s2, s3) {
+    var s = (s1 + s2 + s3) / 3
+    return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+  }
