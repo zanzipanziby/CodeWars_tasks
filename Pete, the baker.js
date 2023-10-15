04e6768a7f3c95a6a40a5e5efbfc4e71c49c9476
ZanziPanziBY <zanzipanziby@gmail.com>
2023-10-15 16:55:29 +0300

    Update Pete, the baker.js

--- Pete, the baker.js
+++ Pete, the baker.js
@@ -2,7 +2,6 @@
 
 Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0. */
 
-
 function cakes(recipe, available) {
     let maxCakes
     for (var ingredient in recipe) {
