55930c300b662c8d04616dbdc5c6b6247a9fd51b
ZanziPanziBY <zanzipanziby@gmail.com>
2023-11-05 22:40:00 +0300

    Update Bouncing Balls.js

--- Bouncing Balls.js
+++ Bouncing Balls.js
@@ -1,11 +1,8 @@
 /**A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
 
 He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
-
-His mother looks out of a window 1.5 meters from the ground.
-
+His mother looks out of a window 1.5 meters from the ground
 How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
-
 Three conditions must be met for a valid experiment:
 Float parameter "h" in meters must be greater than 0
 Float parameter "bounce" must be greater than 0 and less than 1
