501b7ce2627e1fec453a2e785e91ce380aab78af
ZanziPanziBY <zanzipanziby@gmail.com>
2023-11-08 18:49:20 +0300

    Update Printer Errors.js

--- Printer Errors.js
+++ Printer Errors.js
@@ -7,7 +7,6 @@ four times color b, one time color h then one time color a...
 Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
 
 You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
-
 The string has a length greater or equal to one and contains only letters from ato z.
 */
 
