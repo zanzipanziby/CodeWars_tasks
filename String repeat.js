ef02d99caa9d77d16132ab237d8626a6d82e3783
ZanziPanziBY <zanzipanziby@gmail.com>
2023-12-27 18:53:15 +0300

    Update String repeat.js

--- String repeat.js
+++ String repeat.js
@@ -1,6 +1,7 @@
 /**Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times. */
 
 //variant 1
+
 function repeatStr (n, s) {
     return s.repeat(n);
   }
@@ -8,6 +9,7 @@ function repeatStr (n, s) {
 
 
 //variant 2
+
   function repeatStr (n, s) {
     var str="";
     for(var i=0; i < n; i++)
