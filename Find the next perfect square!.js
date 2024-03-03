356b9995b5473854cb97bc3b9dcb0825fbdd56fb
ZanziPanziBY <zanzipanziby@gmail.com>
2024-03-03 22:33:42 +0300

    Update Find the next perfect square!.js

--- Find the next perfect square!.js
+++ Find the next perfect square!.js
@@ -1,5 +1,6 @@
 function findNextSquare(sq) {
     // Return the next square if sq is a perfect square, -1 otherwise
+    
     const number = Math.sqrt(sq);
     return Number.isInteger(number) ? Math.pow(number + 1, 2) : -1;
   }
