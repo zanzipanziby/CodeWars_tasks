b764d099a7bf536cbb94dbafc738ff32bae7d892
ZanziPanziBY <zanzipanziby@gmail.com>
2024-03-11 23:54:23 +0300

    Update Grasshopper - Grade book.js

--- Grasshopper - Grade book.js
+++ Grasshopper - Grade book.js
@@ -7,6 +7,7 @@ Numerical Score	Letter Grade
 60 <= score < 70	'D'
 0 <= score < 60	'F'
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100. */
+
 function getGrade(s1, s2, s3) {
     let num = (s1 + s2 + s3) / 3;
     if (num < 60) {
